874afe490bf3f72782cf2ea02bb7fca9ccf01ac3
Neven <58472164+neven14@users.noreply.github.com>
2022-01-08 09:56:58 +0100

    Add files via upload

---
+++ Drugi_Zad/Drugi_Zad/KlasaB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drugi_Zad
+{
+    class KlasaB
+    {
+        public string BezPrvogIZadnjeg(string stringUnos)
+        {
+            return stringUnos.Substring(1, stringUnos.Length - 2);
+        }
+    }
+}
---
+++ Drugi_Zad/Drugi_Zad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drugi_Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:");
+            string s = Console.ReadLine();
+            KlasaB x = new KlasaB();
+            if (s.Length > 2)
+            {
+                s = x.BezPrvogIZadnjeg(s);
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
